7389e489b52087fd7ddc35456ab413b22992fa03
faruk <mehdiseid51@gmial.com>
2026-09-14 10:22:53 +0300

    fix github pages paths

--- js/api.js
+++ js/api.js
@@ -1,26 +1,24 @@
 import { error_display } from "./ui.js";
 
 async function fetchData() {
-
   try {
     const [topics, tips] = await Promise.all([
+      fetch(`${import.meta.env.BASE_URL}data/topics.json`),
+      fetch(`${import.meta.env.BASE_URL}data/tip.json`)
+    ]);
 
-      fetch("../data/topics.json"),
-      fetch("../data/tip.json")
+    if (!topics.ok || !tips.ok) {
+      throw new Error("Could not load JSON data");
+    }
 
-    ])
-    return [await topics.json(), await tips.json()]
-  }
+    return [
+      await topics.json(),
+      await tips.json()
+    ];
 
-  catch (err) {
-
+  } catch (err) {
     error_display(err);
-
   }
-
-
 }
 
-
-
-export default fetchData
+export default fetchData;
